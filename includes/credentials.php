920af27892e74abf05139e774e726e75d971c4db
mrsxypants <compwizard.t@gmail.com>
2016-08-17 16:15:22 -0400

    This is where we store all our creds

---
+++ includes/credentials.php
@@ -0,0 +1,13 @@
+<?php
+
+// LoadedCommerce
+define("LC_HOSTNAME","localhost");
+define("LC_USERNAME","root");
+define("LC_PASSWORD","root");
+define("LC_DATABASE","loadedcommerce");
+
+// OpenCart
+define("OC_HOSTNAME", "localhost");
+define("OC_USERNAME", "root");
+define("OC_PASSWORD", "root");
+define("OC_DATABASE", "opencarttemp");
