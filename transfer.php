9547ac5b2683f551e8dabee38724028047041cc4
mrsxypants <compwizard.t@gmail.com>
2016-08-19 14:43:48 -0400

    Added some variables to tracking amount migrated

--- transfer.php
+++ transfer.php
@@ -1,14 +1,22 @@
 <?php 
 	// credentials and classes
-	include_once 'includes/classes.php';
+	include_once 'includes/classes.php';	
+
+	// tracking products
+	$Parent_Products		= "0";
+	$Sub_Products			= "0";
 
 	// Make the database connections ooo such naming schema
 	$LoadedCommerce = new LCDB;
 	$OpenCart = new OCDB;
 
 	$Old_Products = $LoadedCommerce->GetProducts();
-
+	
 	while($row = mysqli_fetch_assoc($Old_Products)){
 		$OpenCart->AddProduct($row);
 	}
+
+	$Total_Products = $Parent_Products + $Sub_Products;
+
+	echo "\n\nMigrated:\nParent Products: {$Parent_Products}\nSub Products:{$Sub_Products}\n\nTotal Products:{$Total_Products}\n";
  ?>
